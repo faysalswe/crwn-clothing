4a3356ed5484e0f184a3eac7665e8c063f057f43
Faysal Ahmmad <faysalswe@gmail.com>
2020-01-02 23:24:59 +0600

    updateing our app with a shop page, collection preview and collection item component

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ In the list of topic must study:
 
 ### `React`
 1. nested route experiment
+3. higher order component
 
 ### `JavaScript`
 1. event loop
 2. non blocking
+3. higher order function
+4. foreach synchronous vs asynchronous
--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Hats from './pages/hats/hats.component';
+// import Hats from './pages/hats/hats.component';
+import Shop from './pages/shop/shop.component';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Homepage}/>
-          <Route path='/shop/hats' component={Hats}/>
+          <Route path='/shop' component={Shop}/>
         </Switch>
       </BrowserRouter>
     </div>
